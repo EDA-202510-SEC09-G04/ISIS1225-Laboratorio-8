17cf05a951bd4d5ecd488ff19689f2a521c58afa
DanielGT27-Q <d.galindot@uniandes.edu.co>
2025-04-08 20:42:28 -0500

    agregando remove

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -76,4 +76,67 @@ def get_node (root,key):
         
         return get_node(root['right'],key)
         
-       
+
+
+
+def encontrar_minimo(nodo):
+    
+    actual = nodo
+    
+    while actual['left']:
+        
+        actual = actual['left']
+        
+    return actual
+
+
+
+
+def remove(my_bst,key):
+    
+    my_bst['root'] = remove_node(my_bst['root'],key)
+
+
+def remove_node(root,key):
+    
+    if root == None:
+        
+        return root
+    
+    
+    if  root['key'] > key:
+        
+        root['left'] = remove_node(root['left'],key)
+        
+    elif root['key'] < key:
+        
+        root['right'] = remove_node(root['right'],key)
+        
+    
+    else:
+        
+        # Caso 1: El nodo no tiene hijos
+        
+        if root['left'] is None and root['right'] is None:
+            
+            return None
+        
+        # Caso 2: El nodo solo tiene un hijo
+        
+        if  root['left'] is None:
+            
+            return root['right']
+        
+        elif root['right'] is None:
+            
+            return root['left']
+        
+        #Caso 3: El nodo tiene dos hijos
+        
+        sucesor = encontrar_minimo(root['right'])
+        root['key'] = sucesor['key']
+        root['value'] = sucesor['value']
+        root['right'] = remove_node(root['right'], sucesor['key'])
+    
+    
+    return root
